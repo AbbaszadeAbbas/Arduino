138776450d8a92e522882a612c71cb93e4ccd95b
AbbaszadaAbbas <mr.abbaszada86@gmail.com>
2022-10-22 01:26:14 +0400

    day 2 Led(button)

---
+++ day 2.(Led(button))/Arduino.Code/Arduino.Code.ino
@@ -0,0 +1,15 @@
+int Button = 2;
+int Led = 3;
+int helper;
+void setup() {
+  pinMode(Button, INPUT);
+  pinMode(Led, OUTPUT);
+}
+void loop() {
+  helper = digitalRead(Button);
+  if(helper == HIGH){
+    digitalWrite(Led, HIGH);
+  }else {
+    digitalWrite(Led, LOW);
+  }
+}
